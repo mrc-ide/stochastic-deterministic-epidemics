fec76d533c7ca125661e9ed0d3623039a00ffdc0
edknock <e.knock@imperial.ac.uk>
2024-02-14 12:57:06 +0000

    add sarscov2 run file and pkgdepends for hipercow

--- run/run_sarscov2.R
+++ run/run_sarscov2.R
@@ -1,100 +1,39 @@
-date <- "2021-09-13"
-pars_set <- "2021"
+deterministic <- TRUE
 short_run <- TRUE
 
-regions <- sircovid::regions("england")
+# assumptions can also be any of:
+# crim_infect_high, crim_infect_low
+# crim_hospi_high, crim_hospi_low
+# crim_death_high, crim_death_low
+# booster_ve_high, booster_ve_low
+# alpha_ve_high, alpha_ve_low
+# delta_ve_high, delta_ve_low
+# mu_d_winter, mu_d_summer
+# fixed_si_high, fixed_si_low
+assumptions <- "central"
 
-
-## Run data task
+## 1. sarscov2_data
 orderly2::orderly_run("sarscov2_data")
 
-## Run parameters task
-pars_deterministic <- orderly2::orderly_run(
+## 2. sarscov2_parameters 
+orderly2::orderly_run(
   "sarscov2_parameters",
-  parameters = list(date = date,
-                    pars_set = pars_set,
-                    deterministic = TRUE,
-                    adaptive_proposal = FALSE,
-                    assumptions = "central"))
+  parameters = list(deterministic = deterministic, assumptions = assumptions))
 
-pars_deterministic_adaptive <- orderly2::orderly_run(
-  "sarscov2_parameters",
-  parameters = list(date = date,
-                    pars_set = pars_set,
-                    deterministic = TRUE,
-                    adaptive_proposal = TRUE,
-                    assumptions = "central"))
+## 3. sarscov2_fits
+for (r in sircovid::regions("england")) {
+  orderly2::orderly_run(
+    "sarscov2_fits",
+    parameters = list(region = r,
+                      short_run = short_run,
+                      deterministic = deterministic,
+                      assumptions = assumptions))
+}
 
-pars_stochastic <- orderly2::orderly_run(
-  "sarscov2_parameters",
-  parameters = list(date = date,
-                    pars_set = pars_set,
-                    deterministic = FALSE,
-                    adaptive_proposal = FALSE,
-                    assumptions = "central"))
 
-## Run deterministic fits
-fits_deterministic <-
-  lapply(X = regions,
-         FUN = function(x) {
-           orderly2::orderly_run('sarscov2_fits',
-                                 parameters = list(region = x,
-                                                   date = date,
-                                                   pars_set = pars_set,
-                                                   short_run = short_run,
-                                                   deterministic = TRUE,
-                                                   adaptive_proposal = FALSE,
-                                                   assumptions = "central"))})
-
-combined_deterministic <-
-  orderly2::orderly_run('sarscov2_fits_combined',
-                       parameters = list(date = date,
-                                         pars_set = pars_set,
-                                         short_run = short_run,
-                                         deterministic = TRUE,
-                                         adaptive_proposal = FALSE,
-                                         assumptions = "central"))
-
-## Run deterministic adaptive fits
-fits_deterministic_adaptive <-
-  lapply(X = regions,
-         FUN = function(x) {
-           orderly2::orderly_run('sarscov2_fits',
-                                 parameters = list(region = x,
-                                                   date = date,
-                                                   pars_set = pars_set,
-                                                   short_run = short_run,
-                                                   deterministic = TRUE,
-                                                   adaptive_proposal = TRUE,
-                                                   assumptions = "central"))})
-
-combined_deterministic_adaptive <-
-  orderly2::orderly_run('sarscov2_fits_combined',
-                        parameters = list(date = date,
-                                          pars_set = pars_set,
-                                          short_run = short_run,
-                                          deterministic = TRUE,
-                                          adaptive_proposal = TRUE,
-                                          assumptions = "central"))
-
-## Run deterministic adaptive fits
-fits_stochastic <-
-  lapply(X = regions,
-         FUN = function(x) {
-           orderly2::orderly_run('sarscov2_fits',
-                                 parameters = list(region = x,
-                                                   date = date,
-                                                   pars_set = pars_set,
-                                                   short_run = short_run,
-                                                   deterministic = FALSE,
-                                                   adaptive_proposal = FALSE,
-                                                   assumptions = "central"))})
-
-combined_stochastic <-
-  orderly2::orderly_run('sarscov2_fits_combined',
-                        parameters = list(date = date,
-                                          pars_set = pars_set,
-                                          short_run = short_run,
-                                          deterministic = FALSE,
-                                          adaptive_proposal = FALSE,
-                                          assumptions = "central"))
+## 4. sarscov2_fits_combined
+orderly2::orderly_run(
+  "sarscov2_fits_combined",
+  parameters = list(short_run = short_run,
+                    deterministic = deterministic,
+                    assumptions = assumptions))
